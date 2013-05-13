20de74adac1e1788d902d5ad74d9b88a2b380b7f
Robert Boloc <robertboloc@gmail.com>
2013-05-13 20:50:14 +0200

    added followers method

--- lib/shelfari/book.rb
+++ lib/shelfari/book.rb
@@ -3,14 +3,14 @@ module Shelfari
     def book_by_id(id)
       page = request('/books/'+id.to_s)
       raw_book = page.parser.xpath("//a[@bookid='"+id.to_s+"']").first
-      book = [
+      book = {
         :bookid => id.to_s,
         :editionid => raw_book['editionid'],
         :title => raw_book['title'],
         :author => page.title.gsub(/(.*)by\s/, ''),
         :cover => raw_book.xpath(".//img").first['src'],
         :url => raw_book['href']
-      ]
+      }
       response(book)
     end
     
--- lib/shelfari/user.rb
+++ lib/shelfari/user.rb
@@ -1,16 +1,31 @@
 module Shelfari
   module User
     def user(user)
-      page = request('/'+user)
+      page = request("/#{user}")
       desc = page.parser.at('meta[@name="description"]')
-        user = [
+        user = {
           :description => desc ? desc[:content] : nil,
           :avatar => page.parser.xpath("//a[@class='avatar']//img").first['src'],
           :books => extract_books(page)
-        ]
+        }
       response(user)
     end
     
+    def followers(user)
+      results = Array.new
+      page = request("/#{user}/friends/followers")
+      page.parser.xpath("//ul[@class='grid grid_x_2']//li").each{ |raw_follower|
+        follower = {
+          :user => raw_follower.css('a.name').first['href'].gsub('http://www.shelfari.com/', ''),
+          :name => raw_follower.css('a.name').first.content,
+          :avatar => raw_follower.xpath(".//a[@class='avatar']//img/@src").first.content,
+          :lastseen => raw_follower.xpath(".//span[@class='info']").first.content
+        }
+        results << follower
+      }
+      response(results)
+    end
+
     def now_reading(user)
       page = request("/#{user}/lists/NowReading")
       page.parser.xpath("//script").each do |raw_script|
